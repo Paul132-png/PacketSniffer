ab3b655ddf10aa83894ffcf5e4b765aa55e1f8a3
Ioan Paul Beracko <pauliberacko@gmail.com>
2026-05-24 12:09:55 +0300

    Printare completa + grafic live varianta1
    a

--- main.py
+++ main.py
@@ -2,14 +2,36 @@
 Packet Sniffer pentru Ethernet
 """
 import socket
+import matplotlib.pyplot as plt
+from collections import deque
+import time
 from functions import *
 TAB1 = '\t - '
 TAB2 = '\t\t - '
 TAB3 = '\t\t\t - '
-TAB4 = '\t\t\t\t - '
+
+history = deque(maxlen=60)
+fig, ax = plt.subplots(figsize=(10, 4))
+plt.ion()
+
+def update_graph(packet_count):
+    history.append(packet_count)
+    ax.clear()
+    ax.plot(list(history), color='cyan', linewidth=1.5)
+    ax.fill_between(range(len(history)), list(history), alpha=0.3, color='cyan')
+    ax.set_title("Pachete / secundă")
+    ax.set_ylabel("Packets/s")
+    ax.set_xlabel("Timp (secunde)")
+    ax.set_xlim(0, 60)
+    ax.set_ylim(0, max(history, default=1) * 1.2 + 1)
+    ax.grid(True, alpha=0.3)
+    plt.tight_layout()
+    plt.pause(0.001)
 
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    packet_count = 0
+    last_time = time.time()
 
     while True:
         raw_data, addr = conn.recvfrom(65536)
@@ -22,17 +44,27 @@ def main():
             print(TAB2 + 'Protocol: {}'.format(prot))
             print(TAB3 + 'Source IP: {}, Destination IP: {}'.format(src, dst))
 
+            if prot == 1:
+                icmp_type, code, checksum = icmp_packet(data)
+                print(TAB1 + 'ICMP Packet: ')
+                print(TAB2 + 'Type: {}, Code: {}, Checksum: {}'.format(icmp_type, code, checksum))
 
-
-
-
-
-
-
-
-
-
-
-
+            elif prot == 6:
+                src_port, dst_port, sequence, acknowledgement, offset, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin= tcp_segment(data)
+                print(TAB1 + 'TCP Segment: ')
+                print(TAB2 + 'SRC Port: {}, DST Port: {}'.format(src_port,dst_port))
+                print(TAB2 + 'Sequence: {}, Acknowledgement: {}'.format(sequence,acknowledgement))
+                print(TAB2 + 'Flags: ')
+                print(TAB3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}'.format(flag_urg,flag_ack,flag_psh,flag_rst,flag_syn,flag_fin))
+            elif prot == 17:
+                src_port, dst_port, length, checksum = udp_datagram(data)
+                print(TAB1 + 'UDP Datagram: ')
+                print(TAB2 + 'SRC Port: {}, DST Port: {}'.format(src_port, dst_port))
+                print(TAB2 + 'Length: {}, Checksum: {}'.format(length, checksum))
+        now = time.time()
+        if now - last_time >= 1.0:
+            update_graph(packet_count)
+            packet_count = 0
+            last_time = now
 
 main()

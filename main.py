c4d06d30802ec55e3fa9bbe9eb7808028136d10a
Ioan Paul Beracko <pauliberacko@gmail.com>
2026-05-26 09:12:14 +0300

    Grafic live varianta 2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from collections import deque
 import time
 from functions import *
+
 TAB1 = '\t - '
 TAB2 = '\t\t - '
 TAB3 = '\t\t\t - '
@@ -14,19 +15,31 @@ history = deque(maxlen=60)
 fig, ax = plt.subplots(figsize=(10, 4))
 plt.ion()
 
+line, = ax.plot([], [], color='cyan', linewidth=1.5)
+fill = ax.fill_between([], [], alpha=0.3, color='cyan')
+
+ax.set_title("Pachete / secundă")
+ax.set_ylabel("Packets/s")
+ax.set_xlabel("Timp (secunde)")
+ax.set_xlim(0, 60)
+ax.grid(True, alpha=0.3)
+plt.tight_layout()
+
 def update_graph(packet_count):
+    global fill
     history.append(packet_count)
-    ax.clear()
-    ax.plot(list(history), color='cyan', linewidth=1.5)
-    ax.fill_between(range(len(history)), list(history), alpha=0.3, color='cyan')
-    ax.set_title("Pachete / secundă")
-    ax.set_ylabel("Packets/s")
-    ax.set_xlabel("Timp (secunde)")
-    ax.set_xlim(0, 60)
+    x = list(range(len(history)))
+    y = list(history)
+
+    line.set_data(x, y)
+
+    fill.remove()
+    fill = ax.fill_between(x, y, alpha=0.3, color='cyan')
+
     ax.set_ylim(0, max(history, default=1) * 1.2 + 1)
-    ax.grid(True, alpha=0.3)
-    plt.tight_layout()
-    plt.pause(0.001)
+
+    fig.canvas.draw_idle()
+    fig.canvas.flush_events()
 
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
@@ -50,21 +63,25 @@ def main():
                 print(TAB2 + 'Type: {}, Code: {}, Checksum: {}'.format(icmp_type, code, checksum))
 
             elif prot == 6:
-                src_port, dst_port, sequence, acknowledgement, offset, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin= tcp_segment(data)
+                src_port, dst_port, sequence, acknowledgement, offset, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin = tcp_segment(data)
                 print(TAB1 + 'TCP Segment: ')
-                print(TAB2 + 'SRC Port: {}, DST Port: {}'.format(src_port,dst_port))
-                print(TAB2 + 'Sequence: {}, Acknowledgement: {}'.format(sequence,acknowledgement))
+                print(TAB2 + 'SRC Port: {}, DST Port: {}'.format(src_port, dst_port))
+                print(TAB2 + 'Sequence: {}, Acknowledgement: {}'.format(sequence, acknowledgement))
                 print(TAB2 + 'Flags: ')
-                print(TAB3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}'.format(flag_urg,flag_ack,flag_psh,flag_rst,flag_syn,flag_fin))
+                print(TAB3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}'.format(flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin))
+
             elif prot == 17:
                 src_port, dst_port, length, checksum = udp_datagram(data)
                 print(TAB1 + 'UDP Datagram: ')
                 print(TAB2 + 'SRC Port: {}, DST Port: {}'.format(src_port, dst_port))
                 print(TAB2 + 'Length: {}, Checksum: {}'.format(length, checksum))
+
         now = time.time()
         if now - last_time >= 1.0:
             update_graph(packet_count)
             packet_count = 0
             last_time = now
+        else:
+            packet_count += 1
 
 main()
